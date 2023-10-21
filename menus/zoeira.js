254a0fbbc309e8ace93ed85699b6522a93edef46
CrisAmorim27 <109648665+CrisAmorim27@users.noreply.github.com>
2023-10-21 06:12:43 -0300

    Update zoeira.js

--- menus/zoeira.js
+++ menus/zoeira.js
@@ -2,7 +2,17 @@ const brincadeiras = (prefix) => {
 
 // NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
 
-return `*MENU DESATIVADO KSKSKSKSK*🧴`
+return `*MENU DESATIVADO  POR ENQUANTO..... KSKSKSKSK*🧴
+
+
+
+Mas aproveitando que ta aqui salva o contato do bot na sua agenda, isso ajuda a evitar de eu tomar block ;)
+
+
+
+
+E caso queira alugar fale com meu mestre 554884702848 
+`
 }
 
 exports.brincadeiras = brincadeiras
