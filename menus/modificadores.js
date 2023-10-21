047b539095acbc124806e6c640d73d27e8b77e92
CrisAmorim27 <109648665+CrisAmorim27@users.noreply.github.com>
2023-10-21 06:08:34 -0300

    Update modificadores.js

--- menus/modificadores.js
+++ menus/modificadores.js
@@ -2,7 +2,42 @@ const modificadores = (prefix) => {
 
 // NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
 
-return `*_MENU DESATIVADO_*`
+return `
+
+
+  ᶫᵒᵛᵉᵧₒᵤ ᶫᵒᵛᵉᵧₒᵤ ᶫᵒᵛᵉᵧₒᵤ          
+╭═• ೋ✘๑❤️‍🔥๑✘ೋ •═╮              
+╎     🪩×>Mods<×🪩
+╰═• ೋ✘๑❤️‍🔥๑✘ೋ •═╯
+╎⚘ۣۜۜ͜͡✨ ${prefix}viraraudio "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}videolento "Marcar"
+╎⚘ۜۜ͜͡✨ ${prefix}videorapido "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Videocontrario "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Audiolento "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Audiorapido "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Grave "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Grave2 "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}robot "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Esquilo "Marcar"
+╎⚘ۣۜۜ͜͜͡͡✨ ${prefix}Estourar "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}estourar2 "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Bass "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Bass2 "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Bass3 "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}Vozmenino "Marcar"     
+╎⚘ۣۜۜ͜͡✨ ${prefix}tupai "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}reverse "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}fat "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}deep "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}earrape "Marcar"
+╎⚘ۣۜۜ͜͡✨ ${prefix}falar pt "txt"
+╎⚘ۣۜۜ͜͡✨ ${prefix}faustao "txt"
+╎⚘ۣۜۜ͜͡✨ ${prefix}chapolin "txt"
+╎⚘ۣۜۜ͜͡✨ ${prefix}eminem "txt"
+╎⚘ۣۜۜ͜͡✨ ${prefix}negobam "txt"
+╎⚘♬○♩●✧♩⚘♬●♩✧●♪○♬⚘                          
+╰═• ೋ✧๑💧๑✧ೋ •═╯
+`
 }
 
 exports.modificadores = modificadores
