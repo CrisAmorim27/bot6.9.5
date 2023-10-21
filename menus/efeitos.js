7746287b33f1e2b00c92c9be9c146e5dcb0e4805
CrisAmorim27 <109648665+CrisAmorim27@users.noreply.github.com>
2023-10-21 06:09:27 -0300

    Update efeitos.js

--- menus/efeitos.js
+++ menus/efeitos.js
@@ -2,7 +2,26 @@ const efeitos = (prefix) => {
 
 // NÃO APAGUE ESSE ${prefix}, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as definições, só se quiser apagar a definição completa. 
 
-return `*_MENU DESATIVADO_*`
+return `
+  ᶫᵒᵛᵉᵧₒᵤ ᶫᵒᵛᵉᵧₒᵤ ᶫᵒᵛᵉᵧₒᵤ
+╭═• ೋ❃๑❤️‍🔥๑❃ೋ •═╮ 
+╎   ⛈️×>𝐄𝐅𝐄𝐈𝐓𝐎𝐒<×⛈️
+╰═• ೋ❃๑❤️‍🔥๑❃ೋ •═╯
+╎❃ۣۜۜ͜͡🧩 ${prefix}Legenda "marcar"       
+╎❃ۣۜۜ͜͡🧩 ${prefix}Procurado "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Hitler "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Preso "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Lixo "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Deletef "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Morto "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Lgbt "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}Merda "marcar"     
+╎❃ۣۜۜ͜͡🧩 ${prefix}borrar "marcar" 
+╎❃ۣۜۜ͜͡🧩 ${prefix}noturno "marcar
+╎❃ۣۜۜ͜͡🧩 ${prefix}wasted "marcar"
+╎❃ۣۜۜ͜͡🧩 ${prefix}trigger "marcar
+╰═• ೋ✧๑🩸๑✧ೋ •═╯
+`
 }
 
 exports.efeitos = efeitos
