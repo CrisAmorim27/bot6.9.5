b69989648eb0718787764c8270683eead9b7864e
CrisAmorim27 <109648665+CrisAmorim27@users.noreply.github.com>
2023-10-21 06:02:19 -0300

    Update games.js

--- menus/games.js
+++ menus/games.js
@@ -1,5 +1,49 @@
 const hospedar = (prefix) => {
-return `*_MENU DESATIVADO_*`
+return `*_MENU GAME_*
+
+  ᶫᵒᵛᵉᵧₒᵤ ᶫᵒᵛᵉᵧₒᵤ ᶫᵒᵛᵉᵧₒᵤ          
+╭═• ೋ✘๑❤️‍🔥๑✘ೋ •═╮              
+╎    🪩×>𝐆𝐀𝐌𝐄𝐒<×🪩
+╰═• ೋ✘๑❤️‍🔥๑✘ೋ •═╯
+╎✘ۣۜۜ͜͡🌎 ${prefix}akinator
+╎✘ۣۜۜ͜͡🌎 ${prefix}eununca
+╎✘ۣۜۜ͜͡🌎 ${prefix}forca
+╎✘ۣۜۜ͜͡🌎 ${prefix}jogodavelha
+╎✘ۣۜۜ͜͡🌎 ${prefix}ppt
+╎✘ۣۜۜ͜͡🌎 ${prefix}ttt
+╎✘ۣۜۜ͜͡🌎 ${prefix}jogar
+╎✘ۣۜۜ͜͡🌎 ${prefix}quizanimais
+╎✘ۣۜۜ͜͡🌎 ${prefix}cassino
+╎✘ۣۜۜ͜͡🌎 ${prefix}anagrama
+╎✘ۣۜۜ͜͡🌎 ${prefix}linda (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}lindo (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}gostosa (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}gostoso (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Gay (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Feio (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Corno (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Vesgo (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Bebado (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Beijo (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Matar (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Tapa (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Chute (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}golpista (@) 
+╎✘ۣۜۜ͜͡🌎 ${prefix}Nazista (@)
+╎✘ۣۜۜ͜͡🌎 ${prefix}Chance (fale algo) 
+╎✘ۣۜۜ͜͡🌎 ${prefix}Casal   
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankgay     
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankgado
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankcorno  
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankgostoso
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankgostosa
+╎✘ۣۜۜ͜͡🌎 ${prefix}Ranknazista
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankotakus
+╎✘ۣۜۜ͜͡🌎 ${prefix}Rankpau
+╎✘ۣۜۜ͜͡🌎 ${prefix}pornhub "txt/txt"
+╎✘ۣۜۜ͜͡🌎 ${prefix}googlesg "txt/txt/"
+╎✘ۣۜۜ͜͡🌎 ${prefix}bolsonaro "txt"
+╰═• ೋ✧๑💧๑✧ೋ •═╯`
 }
 
 exports.hospedar = hospedar 
