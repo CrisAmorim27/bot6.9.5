9e43f1dac9149799d5c27c9515187932401d6c7a
Spy <109648665+CrisAmorim27@users.noreply.github.com>
2023-12-03 19:17:41 -0300

    Update infomestre.js

--- menus/infomestre.js
+++ menus/infomestre.js
@@ -4,17 +4,19 @@ const infomestre = (prefix, numerodn, NomeDoBot, hora, tempo, pushname) => {
 //${numerodn} nem ${NomeDoBot} nem ${prefix} só se quiser apagar completo, não coloque nada ${dentro assim} ISSO SÃO DEFINIÇÕES QUE ESTÁ PUXANDO DO settings.json, da pasta dono, só pode altera a base de tudo, menos as , só se quiser apagar a definição completa.
 
 return`
-╭═• ೋ⌑๑๑⌑ೋ •═╮ 
+╭═• ೋ⌑ ๑๑ ⌑ೋ •═╮ 
 ╎☀️∆𝗜𝗡𝗙𝗢𝗠𝗘𝗦𝗧𝗥𝗘∆☀️
-╰═• ೋ⌑๑๑⌑ೋ •═╯
+╰═• ೋ⌑ ๑๑ ⌑ೋ •═╯
 ╎
 ｡･:*:･ﾟ★,｡･:*:･ﾟ☆
 Oiê ${tempo} ฅ^•ﻌ•^ฅ
 ｡･:*:･ﾟ★,｡･:*:･ﾟ☆
 ╎
-※ۣۜۜ͜͡𝙈𝙚𝙨𝙩𝙧𝙚⧽ ${NickDono}
+※ۣۜۜ͜͡𝙈𝙚𝙨𝙩𝙧𝙚⧽ @PapaiSpy
 ╎
-※ۣۜۜ͜͡𝙒𝙝𝙖𝙩𝙨⧽ wa.me/${numerodn}         
+※ۣۜۜ͜͡𝙒𝙝𝙖𝙩𝙨⧽ wa.me/554884702848      
+╎
+※ۣۜۜ͜͡𝙒𝙝𝙖𝙩𝙨⧽ wa.me/554888767821  
 ╎
 ※ۣۜۜ͜͡𝙋𝙧𝙚𝙛𝙞𝙭𝙤⧽﹙${prefix}﹚
 ╎                                    
@@ -23,14 +25,14 @@ Oiê ${tempo} ฅ^•ﻌ•^ฅ
 ※ۣۜۜ͜͡𝙃𝙤𝙧𝙖⧽ ${hora}
 ╎
 ※ۣۜۜ͜͡𝙑𝙤𝙘𝙚̂⧽ ${pushname}
-╎
+╎  
 ╎  _wa.me/554884702848_                                
 ╎    𝐍𝐨𝐛𝐫𝐞 𝐞́ 𝐨 𝐠𝐮𝐞𝐫𝐫𝐞𝐢𝐫𝐨 
 ╎            𝐪𝐮𝐞̂ 𝐬𝐮𝐣𝐚 
 ╎ 𝐬𝐮𝐚 𝐞𝐬𝐩𝐚𝐝𝐚 𝐝𝐞 𝐬𝐚𝐧𝐠𝐮𝐞!🩸      
 ╎ cxxx{}::::::::::::::::::::::::::::> 
 ╎      @𝒇𝒊𝒍𝒐́𝒔𝒐𝒇𝒐 𝒑𝒊𝒕𝒐𝒏🖋
-╰═• ೋ⁑๑💰๑⁑ೋ •═╯
+╰═• ೋ⁑๑ 💰 ๑⁑ೋ •═╯
 `
 }
 
